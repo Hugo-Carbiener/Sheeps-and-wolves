1d4620e9ce2d12dcf3f004018f3bda05abc7695c
Hugo Carbiener <41008899+Hugo-Carbiener@users.noreply.github.com>
2022-07-08 15:26:28 +0200

    Add angle processing for the player position

--- Sheeps-and-wolves/Assets/Scripts/Sheep.cs
+++ Sheeps-and-wolves/Assets/Scripts/Sheep.cs
@@ -14,6 +14,8 @@ public class Sheep : MonoBehaviour
     [SerializeField] private int playerFleeRange = 10;
     [SerializeField] private Rigidbody2D rigidBody;
     [SerializeField] private Transform player;
+    [SerializeField] private int speed = 10;
+    [SerializeField] private int rotationSpeed = 90;
     private SheepState state = SheepState.Idle;
 
     void Start()
@@ -25,7 +27,16 @@ public class Sheep : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((transform.position - player.position).sqrMagnitude <= playerFleeRange * playerFleeRange)
+        Vector2 targetLocation = player.position - transform.position;
+        targetLocation.Normalize();
+
+        float angle = Mathf.Atan2(targetLocation.y, targetLocation.x) / Mathf.PI * 180;
+        if (angle < 0)
+        {
+            angle += 360;
+        }
+        Debug.Log(angle);
+        /*if ((transform.position - player.position).sqrMagnitude <= playerFleeRange * playerFleeRange)
         {
             state = SheepState.Fleeing;
         } else
@@ -35,7 +46,8 @@ public class Sheep : MonoBehaviour
 
         if (state == SheepState.Fleeing)
         {
-            
-        }
+            rigidBody.AddRelativeForce(Vector2.right * speed * Time.deltaTime);
+            transform.rotation = player.rotation;
+        } */
     }
 }
